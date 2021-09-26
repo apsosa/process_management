00cf25b740d61870e2475bc09e6df814d33dd898
pato091 <patriciososa91@gmail.com>
2021-09-26 17:24:10 -0300

    some edits

--- execv_test.c
+++ execv_test.c
@@ -1,11 +1,26 @@
 #include <unistd.h>
- 
+#include <stdio.h>
+
 int main(void) {
   char *binaryPath = "/bin/ls";
   char *args[] = {binaryPath, "-lh", NULL};
  
-  execv(binaryPath, args);
- 
+  printf("I am the parent.Process ID:%d\n",getpid());
+  pid_t hijo1 = fork();
+  if (hijo1 == 0)
+  {
+    /* code */
+    printf("I am the first child.Process ID:%d\n",getpid());
+    execv(binaryPath, args);
+    pid_t hijo2 = fork();
+    if (hijo2 == 0)
+    {
+      /* code */
+      printf("I am the second child.Process ID:%d\n",getpid());
+    }
+    
+  }
+  printf("Ending the Main function\n");
   return 0;
 }
 
